417ee95b47a8ce0d15e3f137961da1a425134642
“sarahjessro” <sarahjess444@gmail.com>
2025-11-06 18:39:58 -0500

    Added case/switch for ALU operations

--- ProjectFiles/project.c
+++ ProjectFiles/project.c
@@ -5,6 +5,54 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
+    //Do operations shown in table (instructions) and output correct result
+    //case 1..2..3..4 etc ?
+    switch (ALUControl)
+    {
+    //Addition
+    case 0x0:
+        ALUresult = A+B;
+        break;
+    
+    //Subtraction
+    case 0x1:
+        ALUresult = A-B;
+        break;
+    
+    
+    case 0x2:
+        //if A < B, Z = 1; otherwise, Z = 0
+        break;
+
+    case 0x3:
+        //if A < B, Z = 1; otherwise, Z = 0 (A and B are unsigned integers)
+        break;
+        
+    case 0x4:
+        //Z = A AND B
+        ALUresult = A & B;
+        break;    
+    
+    case 0x5:
+        //Z = A OR B
+        ALUresult = A | B;
+        break;
+
+    case 0x6:
+        //Z = SHIFT B LEFT BY 16 BITS
+        ALUresult = B << 16;
+        break;
+        
+    case 0x7:
+        //Z = NOT A
+        ALUresult = ~A;
+        break;
+        
+        
+
+    default:
+        break;
+    }
 
 }
 
